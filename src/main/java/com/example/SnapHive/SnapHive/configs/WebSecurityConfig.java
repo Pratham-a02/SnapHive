98cd0e88af9065abc0c0a0cd078d3ecf91a9345b
Pratham Pahad <prathampahad012@gmail.com>
2025-07-18 10:44:01 +0530

    Update WebSecurityConfig to allow frontend controller access

--- src/main/java/com/example/SnapHive/SnapHive/configs/WebSecurityConfig.java
+++ src/main/java/com/example/SnapHive/SnapHive/configs/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/index.html", "/auth/**", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/**/*.html", "/auth/**", "/css/**", "/js/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
